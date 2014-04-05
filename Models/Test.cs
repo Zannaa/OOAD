ed637b51d781771d4b8ea9a06aae42658e185949
Zannaa <zana_14t@hotmail.com>
2014-04-05 15:57:21 +0200

    testna klasa

---
+++ Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Test
+    {
+        public int Test { get; set; }
+
+        public Test(int test)
+        {
+            this.Test = test;
+        }
+    }
+}
