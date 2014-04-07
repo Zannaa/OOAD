166870dd9cd09d9cc76cb0786fda765818a14091
azramahmutovic <azramah6@gmail.com>
2014-04-07 16:04:44 +0200

    Klase Racun i PrehrambeniProdukt

---
+++ Models/PrehrambeniProdukt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PrehrambeniProdukt
+    {
+        #region Properties
+        /// <summary>
+        /// Tip produkta
+        /// </summary>
+        public string Tip { get; set; }
+
+        /// <summary>
+        /// Cijena produkta
+        /// </summary>
+        public double Cijena { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Prazan konstruktor
+        /// </summary>
+        public PrehrambeniProdukt()
+        {
+
+        }
+        /// <summary>
+        /// Konstruktor klase PrehrambeniProdukt
+        /// </summary>
+        /// <param name="tip">Tip produkta</param>
+        /// <param name="cijena">Cijena produkta</param>
+        public PrehrambeniProdukt(string tip, double cijena)
+        {
+            this.Tip = tip;
+            this.Cijena = cijena;
+        }
+        #endregion
+    }
+}
---
+++ Models/Racun.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Racun
+    {
+        #region Properties
+        /// <summary>
+        /// Prodavac koji izdaje racun
+        /// </summary>
+        public ProdavacHrane Prodavac { get; set; }
+
+        /// <summary>
+        /// Produkti na racunu 
+        /// </summary>
+        public List<PrehrambeniProdukt> Produkti { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Prazan konstruktor
+        /// </summary>
+        public Racun()
+        {
+
+        }
+        /// <summary>
+        /// Konstruktor klase Racun
+        /// </summary>
+        /// <param name="prodavac">Prodavac hrane</param>
+        /// <param name="produkt">Produkt koji se kupuje</param>
+        public Racun(ProdavacHrane prodavac, PrehrambeniProdukt produkt)
+        {
+            this.Prodavac = prodavac;
+            Produkti = new List<PrehrambeniProdukt>();
+            Produkti.Add(produkt);
+        }
+  
+        /// <summary>
+        /// Konstruktor klase s listom proizvoda
+        /// </summary>
+        /// <param name="prodavac">Prodavac hrane</param>
+        /// <param name="produkti">Lista proizvoda koji se kupuju</param>
+        public Racun(ProdavacHrane prodavac, List<PrehrambeniProdukt> produkti) 
+        {
+            this.Prodavac = prodavac;
+            this.Produkti = produkti;
+        }
+        #endregion
+    }
+}
