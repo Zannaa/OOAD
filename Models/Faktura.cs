4b2f0724cb0d5147585708e5e067792cd617cbfb
azramahmutovic <azramah6@gmail.com>
2014-04-07 19:57:25 +0200

    Klase Faktura i Karta

---
+++ Models/Faktura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Faktura
+    {
+        #region Properties
+        /// <summary>
+        /// ID fakture
+        /// </summary>
+        public int Sifra { get; set; }
+        /// <summary>
+        /// Vrijeme fakturisanja
+        /// </summary>
+        public DateTime Vrijeme { get; set; }
+        /// <summary>
+        /// Menadzer koji je odobrio fakturu
+        /// </summary>
+        public Menadzer Menadzer { get; set; }
+        #endregion
+    }
+}
---
+++ Models/Karta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Karta
+    {
+        #region Properties
+        /// <summary>
+        /// Prodavac karte
+        /// </summary>
+        public ProdavacKarata Prodavac { get; set; }
+        /// <summary>
+        /// Kupac karte
+        /// </summary>
+        public Kupac Kupac { get; set; }
+        /// <summary>
+        /// Termin filma
+        /// </summary>
+        public Projekcija Termin { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Prazan konstruktor
+        /// </summary>
+        public Karta()
+        {
+
+        }
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="prodavac">Klasa prodavaca karte</param>
+        /// <param name="kupac">Kupac karte</param>
+        /// <param name="termin">Termin filma</param>
+        public Karta(ProdavacKarata prodavac, Kupac kupac, Projekcija termin )
+        {
+            this.Prodavac = prodavac;
+            this.Kupac = kupac;
+            this.Termin = termin;
+
+        }
+        #endregion
+
+    }
+}
