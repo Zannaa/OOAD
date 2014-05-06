f8b3b55974a4a6e12f476551d4f7879e0eab498f
azramahmutovic <azramah6@gmail.com>
2014-05-06 02:42:21 +0200

    ProdavacHraneDAO

---
+++ DAO/ProdavacHraneDAO.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO.Interfaces;
+using Models;
+using DAL;
+using System.Data;
+
+namespace DAO
+{
+    public class ProdavacHraneDAO : IRepository<ProdavacHrane>
+    {
+
+        private DatabaseManager manager;
+
+        public ProdavacHraneDAO()
+        {
+            this.manager = DatabaseManager.Instance;
+        }
+
+
+        public long create(ProdavacHrane prodavacHrane)
+        {
+            string exec = "INSERT INTO finansijskiMenadzer VALUES(" + prodavacHrane.Pult + "', " + prodavacHrane.Ime + ", '" + prodavacHrane.Prezime + "', " + prodavacHrane.Jmbg + ", '" + prodavacHrane.Id_uposlenika + "', " + prodavacHrane.Koeficijent + ")";
+
+            return manager.ExecuteSqlCommandToIntForCreate(exec);
+
+        }
+
+        public ProdavacHrane read(ProdavacHrane prodavacHrane
+        {
+
+            return getById(prodavacHrane.Id_uposlenika);
+        }
+
+        public ProdavacHrane update(ProdavacHrane prodavacHrane)
+        {
+            string pult = prodavacHrane.Pult;
+            string ime = prodavacHrane.Ime;
+            string prezime = prodavacHrane.Prezime;
+            string jmbg = prodavacHrane.Jmbg;
+            int id = prodavacHrane.Id_uposlenika;
+            double koeficijent = prodavacHrane.Koeficijent;
+
+            string exec = "UPDATE prodavacKarata SET pult = '" + pult + "', ime = '" + ime + "', prezime = '" + prezime + "', jmbg = '" + jmbg + ", id = " + id + "', koeficijent = " + koeficijent;
+            exec += " WHERE id = " + id;
+
+            int affectedRows = manager.ExecuteSqlCommandToInt(exec);
+            return prodavacHrane;
+        }
+
+        public void delete(ProdavacHrane prodavacHrane)
+        {
+            int id = prodavacHrane.Id_uposlenika;
+
+
+            int affectedRows = manager.ExecuteSqlCommandToInt("DELETE FROM prodavacHrane WHERE id = " + id);
+
+        }
+
+        public ProdavacHrane getById(int id)
+        {
+
+            DataSet data = manager.ExecuteSqlCommandToDataSet("SELECT * FROM prodavacHrane where id = " + id);
+
+            foreach (DataRow dataRow in data.Tables[0].Rows)
+            {
+                ProdavacHrane prodavacHrane = new ProdavacHrane(
+                    Convert.ToString(dataRow["pult"]),
+                    Convert.ToString(dataRow["ime"]),
+                    Convert.ToString(dataRow["prezime"]),
+                    Convert.ToString(dataRow["jmbg"]),
+                    Convert.ToInt32(dataRow["id"]),
+                    Convert.ToDouble(dataRow["koeficijent"])
+                );
+
+                return prodavacHrane;
+            }
+
+
+            return null;
+        }
+        public List<ProdavacHrane> getAll()
+        {
+            DataSet data = manager.ExecuteSqlCommandToDataSet("SELECT * FROM prodavacHrane");
+
+            List<ProdavacHrane> prodavaciHrane = new List<ProdavacHrane>();
+            foreach (DataRow dataRow in data.Tables[0].Rows)
+            {
+                ProdavacHrane prodavacHrane = new ProdavacHrane(
+                    Convert.ToString(dataRow["pult"]),
+                    Convert.ToString(dataRow["ime"]),
+                    Convert.ToString(dataRow["prezime"]),
+                    Convert.ToString(dataRow["jmbg"]),
+                    Convert.ToInt32(dataRow["id"]),
+                    Convert.ToDouble(dataRow["koeficijent"])
+                );
+
+                prodavaciHrane.Add(prodavacHrane);
+            }
+
+            return prodavaciHrane;
+        }
+
+    }
+}
