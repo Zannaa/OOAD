9daa1e95bf9abac156139860b6f5dc373d2736e3
Zannaa <zana_14t@hotmail.com>
2014-05-31 09:07:07 +0200

    LOGIN

--- DejaView/MainWindow.xaml.cs
+++ DejaView/MainWindow.xaml.cs
@@ -12,23 +12,76 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Models;
+using DAL;
+using DAO;
+using DAO.Interfaces;
+
 
 namespace DejaView
-{
+{   
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {    private string user=null;
+        private string pass=null;
+        private string sesija=null ;
+
         public MainWindow()
         {
            // InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Login l =new Login();
-            l.Show();
+      
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {  
+            /*user=text1.Text;
+            pass=text2.Text;
+
+            if (r1.IsChecked==true) { 
+            
+             ProdavacKarata p=new ProdavacKarata() ;
+            ProdavacKarataDAO d =new ProdavacKarataDAO() ;
+            p = d.getById(Convert.ToInt32(user));
+            if (p.Jmbg == pass && p != null) sesija = "logovan";
+
+            
+            }
+            else if    (r2.IsChecked == true) {
+                ProdavacHrane p = new ProdavacHrane();
+                ProdavacHraneDAO d = new ProdavacHraneDAO();
+                p = d.getById(Convert.ToInt32(user));
+                if (p.Jmbg == pass && p != null) sesija = "logovan";
+            }
+             else if    (r2.IsChecked == true) {
+                 Menadzer p = new Menadzer();
+                 MenadzerDAO d = new MenadzerDAO();
+                 p = d.getById(Convert.ToInt32(user));
+                 if (p.Jmbg == pass && p != null) sesija = "logovan";
+             }
+             else {
+           
+                 FinansijskiMenadzer p = new FinansijskiMenadzer();
+                 FinansijskiMenadzerDAO d = new FinansijskiMenadzerDAO();
+                 p = d.getById(Convert.ToInt32(user));
+                 if (p.Jmbg == pass && p != null) sesija = "logovan";
+             }
+
+            if (sesija == "logovan")
+            {
+                sesija = null;
+            }
+            //   azrinprozor.Show() ;}
+            else
+            {*/
+               MainWindow m = new MainWindow();
+                m.Show(); 
+            
+           // }
+            
+
         }
     }
 }
