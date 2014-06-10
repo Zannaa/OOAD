411891efaf589ee777b1a0d0b507fdb4141e5b4d
Zannaa <zana_14t@hotmail.com>
2014-06-10 06:54:29 +0200

    Log in

--- DejaView/MainWindow.xaml.cs
+++ DejaView/MainWindow.xaml.cs
@@ -19,73 +19,105 @@ using DAO.Interfaces;
 
 
 namespace DejaView
-{   
-   
+{
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {    private string user=null;
-        private string pass=null;
-        private string sesija=null ;
+    {
+        private string user = null;
+        private string pass = null;
+        private string sesija = null;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-      
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {  
-            /*user=text1.Text;
-            pass=text2.Text;
 
-            if (r1.IsChecked==true) { 
-            
-             ProdavacKarata p=new ProdavacKarata() ;
-            ProdavacKarataDAO d =new ProdavacKarataDAO() ;
-            p = d.getById(Convert.ToInt32(user));
-            if (p.Jmbg == pass && p != null) sesija = "logovan";
+        /*private void Button_Click(object sender, RoutedEventArgs e)
+                {   
+                    MessageBox.Show("Test"); }
 
+                   /* user=text1.Text;
+                    pass = text2.Password;
+
+                    if (r1.IsChecked==true) { 
             
-            }
-            else if    (r2.IsChecked == true) {
-                ProdavacHrane p = new ProdavacHrane();
-                ProdavacHraneDAO d = new ProdavacHraneDAO();
-                p = d.getById(Convert.ToInt32(user));
-                if (p.Jmbg == pass && p != null) sesija = "logovan";
-            }
-             else if    (r2.IsChecked == true) {
-                 Menadzer p = new Menadzer();
-                 MenadzerDAO d = new MenadzerDAO();
-                 p = d.getById(Convert.ToInt32(user));
-                 if (p.Jmbg == pass && p != null) sesija = "logovan";
-             }
-             else {
+                     ProdavacKarata p=new ProdavacKarata() ;
+                    ProdavacKarataDAO d =new ProdavacKarataDAO() ;
+                    p = d.getById(Convert.ToInt32(user));
+                    if (p.Jmbg == pass && p != null) sesija = "logovan";
+                    ProdajaWindow w = new ProdajaWindow();
+                    w.Show();
+            
+                    }
+                   /* else if    (r2.IsChecked == true) {
+                        ProdavacHrane p = new ProdavacHrane();
+                        ProdavacHraneDAO d = new ProdavacHraneDAO();
+                        p = d.getById(Convert.ToInt32(user));
+                        if (p.Jmbg == pass && p != null) sesija = "logovan";
+                    }
+                     else if    (r2.IsChecked == true) {
+                         Menadzer p = new Menadzer();
+                         MenadzerDAO d = new MenadzerDAO();
+                         p = d.getById(Convert.ToInt32(user));
+                         if (p.Jmbg == pass && p != null) sesija = "logovan";
+                     }
+                     else {
            
-                 FinansijskiMenadzer p = new FinansijskiMenadzer();
-                 FinansijskiMenadzerDAO d = new FinansijskiMenadzerDAO();
-                 p = d.getById(Convert.ToInt32(user));
-                 if (p.Jmbg == pass && p != null) sesija = "logovan";
-             }
+                         FinansijskiMenadzer p = new FinansijskiMenadzer();
+                         FinansijskiMenadzerDAO d = new FinansijskiMenadzerDAO();
+                         p = d.getById(Convert.ToInt32(user));
+                         if (p.Jmbg == pass && p != null) sesija = "logovan";
+                     }
 
-            if (sesija == "logovan")
+                    if (sesija == "logovan")
+                    {
+                        sesija = null;
+                    }
+                    //   azrinprozor.Show() ;}
+                    else
+                    {
+                       MainWindow m = new MainWindow();
+                        m.Show();  
+            
+                    }
+                }*/
+
+
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            user = text1.Text;
+            pass = text2.Password;
+
+            if (r1.IsChecked == true)
             {
-                sesija = null;
+
+                ProdavacKarata p = new ProdavacKarata();
+                ProdavacKarataDAO d = new ProdavacKarataDAO();
+                p = d.getById(Convert.ToInt32(user));
+                //  if (p == null) MessageBox.Show("Ne postoji korisnik!");
+                if (p.Jmbg == pass && p != null) sesija = "logovan";
+                ProdajaWindow w = new ProdajaWindow();
+                w.Show();
+
             }
-            //   azrinprozor.Show() ;}
             else
             {
-               MainWindow m = new MainWindow();
-                m.Show(); 
-            */
-            }
-            
+                MainWindow m = new MainWindow();
+                m.Show();
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           // Login l =new Login();
-           // l.Show();
+            }
         }
+
+
     }
 }
